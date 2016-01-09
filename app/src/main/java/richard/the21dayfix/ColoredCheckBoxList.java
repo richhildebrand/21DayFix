bdbfe1694e32c248793c4e3cda1db4b9ebc5835f
richhildebrand <richhildebrand@gmail.com>
2016-01-09 02:18:11 -0500

    removed old files

--- app/src/main/java/richard/the21dayfix/ColoredCheckBoxList.java
+++ app/src/main/java/richard/the21dayfix/ColoredCheckBoxList.java
@@ -1,7 +1,6 @@
 package richard.the21dayfix;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
