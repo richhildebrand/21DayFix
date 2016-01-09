d8bef8496bcac41b84c17b9a549f1feca59ee87d
richhildebrand <richhildebrand@gmail.com>
2016-01-09 01:01:40 -0500

    pass the checkbox color as a custom attribute

--- app/src/main/java/richard/the21dayfix/ColoredCheckBox.java
+++ app/src/main/java/richard/the21dayfix/ColoredCheckBox.java
@@ -1,37 +1,42 @@
 package richard.the21dayfix;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 
-public class ColoredCheckBox extends LinearLayout
-{
-    public ColoredCheckBox(Context context)
-    {
-        super(context);
-        initControl(context);
-    }
+public class ColoredCheckBox extends LinearLayout {
 
     public ColoredCheckBox(Context context, AttributeSet attrs)
     {
         super(context, attrs);
-        initControl(context);
-
-        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.ColoredCheckBoxElement, 0, 0);
-        String color = a.getString(R.styleable.ColoredCheckBoxElement_CheckBoxColor);
-        String colorAgain = color;
+        inflate(context);
+        String checkBoxColor = getColor(context, attrs);
+        applyColors(checkBoxColor);
     }
 
-/*    public GhostSurfaceCameraView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init();
-    }*/
-
-    private void initControl(Context context)
+    private void inflate(Context context)
     {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.widget_colored_check_box, this);
+    }
+
+    private String getColor(Context context, AttributeSet attrs) {
+        TypedArray coloredCheckBoxAttributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.ColoredCheckBoxElement, 0, 0);
+        return coloredCheckBoxAttributes.getString(R.styleable.ColoredCheckBoxElement_CheckBoxColor);
+    }
+
+    private void applyColors(String color) {
+        applyColor(R.id.checkbox_one, color);
+    }
+
+    private void applyColor(Integer id, String color) {
+        CheckBox checkBoxOne = (CheckBox) findViewById(id);
+        int textColor = Color.parseColor(color);
+        checkBoxOne.setButtonTintList(ColorStateList.valueOf(textColor));
     }
 }
